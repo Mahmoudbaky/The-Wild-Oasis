e6626ca01a2b467d173382b4177e37b8742ec4a2
Mahmoud Saleh <ma7mouudbaky@gmail.com>
2025-11-08 20:12:01 +0300

    Fix background color syntax in Login component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
 function Login() {
   return (
-    <div className="min-h-screen grid grid-cols-[48rem] content-center justify-center gap-[3.2rem] bg-[var(--color-grey-50)]">
+    <div className="min-h-screen grid grid-cols-[48rem] content-center justify-center gap-[3.2rem] bg-(--color-grey-50)">
       Login
     </div>
   );
